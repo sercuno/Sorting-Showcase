ee19237da982ee69df16e114c11af537fbdbcab0
sercuno <sercuno@users.noreply.github.com>
2025-04-21 12:18:25 +0200

    Update 0.0.1

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 // Selection Sort 
 function selectionSort(arr) {
+    if (arr.length === 0) {
+        return "empty";  
+    }
     let n = arr.length;
     for (let i = 0; i < n - 1; i++) {
         let min_indx = i;
@@ -20,31 +23,44 @@ function selectionSort(arr) {
     }
     return arr;
 }
+
 document.getElementById("selectionSort").addEventListener('click', function(event) {
     event.preventDefault();
-    document.body.innerHTML = " ";
+    
     document.body.innerHTML = `
     <h1>Selection Sort</h1>
     <form id="selectionSortID">
-        <label for="txt-input">Text(space in-between required) </label>
+        <label for="txt-input">Text (space in-between required):</label>
         <input type="text" id="txt-input" placeholder=".e.g. 59 94 120 21"><br><br>
         <button type="submit">Send-inn</button>
     </form>
     <p id="resultat"></p>
     <p id="time"></p>`;
 
-    const form = document.getElementById("selectionSortID")
+    const form = document.getElementById("selectionSortID");
+    const input = document.getElementById("txt-input");
+    const paragraphRestultat = document.getElementById("resultat");
+    const paragraphTime = document.getElementById("time");
+
     form.addEventListener('submit', function(event) {
         event.preventDefault();
-        let paragraphRestultat = document.getElementById("resultat");
-        let paragraphTime = document.getElementById("time");
+
         paragraphRestultat.innerHTML = "";
         paragraphTime.innerHTML = "";
-        const txtInputValue = document.getElementById("txt-input");
-        let txtArr = txtInputValue.value.split(" ");
-        const start = performance.now();
-        paragraphRestultat.innerHTML = `<strong>Sorted Text:</strong> ${selectionSort(txtArr).join(" ")}`;
-        const end = performance.now();
-        paragraphTime.innerHTML = `<strong>Time Taken:</strong> ${end - start} ms`;
-    })
+
+        const value = input.value.trim();
+
+        if (value !== "") {
+            let txtArr = value.split(" ");
+            const start = performance.now();
+            let result = selectionSort(txtArr);
+            const end = performance.now();
+
+            paragraphRestultat.innerHTML = `<strong>Sorted Text:</strong> ${result.join(" ")}`;
+            paragraphTime.innerHTML = `<strong>Time Taken:</strong> ${(end - start).toFixed(2)} ms`;
+        } else {
+            paragraphRestultat.innerHTML = `<strong>Sorted Text: Empty</strong>`;
+            paragraphTime.innerHTML = `<strong>Time Taken:</strong> 0 ms`;
+        }
+    });
 });
